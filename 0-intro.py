ff600d9c46712ff0ee168f1f5016d03ed4c861c6
pratap-singh-chouhan <pratapbanna963@gamil.com>
2025-09-24 14:08:30 +0530

    initial commit

---
+++ 0-intro.py
@@ -0,0 +1,13 @@
+'''
+
+Git is a version control system
+it helps developes keep track of changes in therire code
+collaborate with others and roll back to earlier versions
+if something goes wrong.
+
+think of git like a time machine for your code:
+it seves snapshot of your project ( called commits).
+you can go back to old versions anytime
+you can work with teams without overwriting each others code.
+
+'''
